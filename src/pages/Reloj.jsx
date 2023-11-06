3e5db22a8bd7350860d2b0242e256798b4a2a566
Pablo <plabartalarrauri@gmail.com>
2023-11-06 20:24:21 +0000

    hemos realizao los cambios, he quitado las benditas llaves del import en App.jsx y al fin aparece el botón.

---
+++ src/pages/Reloj.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function Reloj() {
+    const [hora, setHora] = useState(new Date().toLocaleTimeString());
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setHora(new Date().toLocaleTimeString());
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return <div>Hora actual: {hora}</div>;
+}
+
+export default Reloj;
+
+
