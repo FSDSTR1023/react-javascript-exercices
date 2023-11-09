0b5b2940116e8044f5e318116cd879bf497cdc6c
Juan Mencía <juamm99@gmail.com>
2023-11-09 18:52:42 +0100

    Método forEach

--- src/components/MetodoForEach.jsx
+++ src/components/MetodoForEach.jsx
@@ -6,10 +6,12 @@ los elementos de un array y mostrar el resultado.
 
 export const MetodoForEach = () => {
   const numeros = [1, 2, 3, 4, 5];
+  let suma=0;
+  numeros.forEach((number)=>suma+=number);
   
   return (
     <>
-      Escribe tu solución aquí
+      Suma total de los elementos: {suma}
     </>
   );
 };
