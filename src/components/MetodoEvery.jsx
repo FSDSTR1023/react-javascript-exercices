97ee69dff8ab919536bfcb0b07642483a0ba4638
Juan Mencía <juamm99@gmail.com>
2023-11-09 18:15:31 +0100

    Método every

--- src/components/MetodoEvery.jsx
+++ src/components/MetodoEvery.jsx
@@ -6,10 +6,11 @@ mayores que 10 utilizando el método every().
 
 export const MetodoEvery = () => {
   const numeros = [11, 12, 13, 14];
+  const numeros2 = [1, 2, 3, 4];
 
   return (
     <>
-      Escribe tu solución aquí
+      Los números son {(numeros.every((number)=>number>10))? "mayores":"menores"} que 10
     </>
   );
 };
