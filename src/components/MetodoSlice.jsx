eeead9c58df9850281a587ab317b3d07c9d197ce
Juan Mencía <juamm99@gmail.com>
2023-11-09 20:40:47 +0100

    Método slice

--- src/components/MetodoSlice.jsx
+++ src/components/MetodoSlice.jsx
@@ -7,9 +7,16 @@ hasta el índice 5 utilizando el método slice().
 export const MetodoSlice = () => {
   const numeros = [0, 1, 2, 3, 4, 5, 6];
 
+  numeros.slice();
+
   return (
     <>
-      Escribe tu solución aquí
+      Elementos extraídos (índice 2, 3, 4 y 5):
+      <ul>
+        {
+          numeros.slice(2,6).map((number)=> <li>{number}</li>)
+        }
+      </ul>
     </>
   );
 };
