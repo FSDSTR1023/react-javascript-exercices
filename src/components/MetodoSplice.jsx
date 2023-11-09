297911e7f0cfb77ef2bb90a2ffb88816f01fab31
Juan Mencía <juamm99@gmail.com>
2023-11-09 20:52:58 +0100

    Método splice

--- src/components/MetodoSplice.jsx
+++ src/components/MetodoSplice.jsx
@@ -7,9 +7,13 @@ muestra el array modificado utilizando el método splice().
 export const MetodoSplice = () => {
   const numeros = [0, 1, 2, 3, 4, 5, 6];
 
+  numeros.splice(3,2);
   return (
     <>
-      Escribe tu solución aquí
+      Array final:
+      <ul>
+        {numeros.map((number)=> <li>{number}</li>)}
+      </ul>
     </>
   );
 };
